f8990bf008828afbf196cb80b459bd5fe05455f0
kunghwan <ysw03031@naver.com>
2025-04-18 17:19:01 +0900

    init

---
+++ src/app/auth/signup/page.tsx
@@ -0,0 +1,15 @@
+const Signup = () => {
+  return (
+    <>
+      <div className="border w-100 flex justify-center  h-100 rounded-2xl mx-auto">
+        <input
+          type="text"
+          className="border-b h-20 w-full placeholder:text-2xl placeholder:text-black placeholder:mr-4"
+          placeholder="이름"
+        />
+      </div>
+    </>
+  );
+};
+
+export default Signup;
