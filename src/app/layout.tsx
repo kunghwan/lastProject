e732ae4b55db041cfbae3a110d0ef602d921f29a
허승이 <hse@heoseung-iui-MacBookAir.local>
2025-04-22 10:33:45 +0900

    readme.md

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Loaiding from "@/lib/Loading/page";
+import Loaiding from "@/components/Loading/page";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
