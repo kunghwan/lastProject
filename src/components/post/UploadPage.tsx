f47268ac231efad658360815e905525a7209c4af
허승이 <hse@heoseung-iui-MacBookAir.local>
2025-04-22 14:54:20 +0900

    upload page ing

--- src/components/post/UploadPage.tsx
+++ src/components/post/UploadPage.tsx
@@ -3,6 +3,9 @@ import { Post } from "@/types/post";
 import React, { useCallback, useState } from "react";
 import { twMerge } from "tailwind-merge";
 import FileItem from "./FileItem";
+import { v4 } from "uuid";
+import { dbService, FBCollection } from "@/lib";
+import { useRouter } from "next/navigation";
 
 interface Tag {
   id: string;
@@ -11,12 +14,13 @@ interface Tag {
 
 interface UploadPostProps extends Post {
   imgs: [];
-  onChangeFile: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  onDeleteFile: () => void;
+  onChangeFile?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onDeleteFile?: () => void;
+
   tags: Tag[] | null;
 }
 
-const initialState: Post = {
+const initialState: UploadPostProps = {
   id: "",
   uid: "",
   userNickname: "",
@@ -33,15 +37,27 @@ const initialState: Post = {
   bookmarked: [],
   isLiked: false,
   createdAt: Date(),
+  imgs: [],
+  tags: null,
 };
 
 const UploadPostPage = () => {
-  const [post, setPost] = useState<Post>(initialState);
+  const [post, setPost] = useState<UploadPostProps>(initialState);
   const [files, setFiles] = useState<File[]>([]);
   const [title, setTitle] = useState("");
-  const [tags, setTags] = useState<string[]>([]);
+  const [tags, setTags] = useState<Tag[]>([]);
   const [tag, setTag] = useState("");
   const [desc, setDesc] = useState("");
+  const navi = useRouter();
+
+  const onChangeFiles = useCallback(
+    (items: FileList) => {
+      for (const file of items) {
+        setFiles((prev) => [...prev, file]);
+      }
+    },
+    [files]
+  );
 
   const onSubmit = useCallback(
     async (e) => {
@@ -49,19 +65,29 @@ const UploadPostPage = () => {
       if (title.length === 0 || title.trim() === "") {
         return alert("제목을 입력해주세요.");
       }
-
-      try {
-      } catch (error: any) {
-        return alert(error.message);
+      if (desc.length === 0 || desc.trim() === "") {
+        return alert("내용을 입력해주세요.");
       }
+
+      // try {
+      //   const ref = await dbService.collection(FBCollection.POSTS).doc(post.id);
+      //   const snap = await ref.set(post)
+      // } catch (error: any) {
+      //   return alert(error.message);
+      // }
     },
     [title]
   );
+
   return (
-    <div>
-      <div className="bg-[rgba(250,255,254)] px-5 border h-screen">
-        <h1 className="text-3xl font-bold text-black">새글작성</h1>
-        <form action="" onSubmit={onSubmit} className="flex flex-col gap-2">
+    <div className="mt-5 max-w-300 mx-auto bg-[rgba(250,255,254)] p-5  border h-screen relative">
+      <h1 className="text-3xl font-bold text-black">새글작성</h1>
+      <form
+        action=""
+        onSubmit={onSubmit}
+        className="flex flex-col gap-2 lg:grid lg:grid-cols-2 lg:gap-5"
+      >
+        <div className="flex flex-col gap-2">
           <input
             type="text"
             value={title}
@@ -73,53 +99,83 @@ const UploadPostPage = () => {
             name=""
             id=""
             placeholder="소개하고 싶은 관광지의 소개글이나 리뷰를 작성해주세요."
-            className={twMerge("h-20", input)}
+            className={twMerge("h-20 resize-none", input)}
             value={desc}
             onChange={(e) => setDesc(e.target.value)}
-          ></textarea>
-
+          />
+          <div>
+            <ul className="flex items-center gap-2.5">
+              {files.map((file) => (
+                <li key={file.name}>
+                  <FileItem
+                    file={file}
+                    onDeleteFiles={() =>
+                      setFiles((prev) =>
+                        prev.filter((item) => item.name !== file.name)
+                      )
+                    }
+                  />
+                </li>
+              ))}
+              <li>
+                <FileItem onChangeFiles={onChangeFiles} />
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div>
           <input
             type="text"
             value={tag}
             onChange={(e) => setTag(e.target.value)}
             onKeyDown={(e) => {
               const { key, nativeEvent } = e;
-              if (key === "Enter" && !nativeEvent.isComposing) {
+              if (key === "Shift" && !nativeEvent.isComposing) {
                 const formattedTag = tag.startsWith("#") ? tag : `#${tag}`;
-                setTags((prev) => [...prev, formattedTag]);
+                const newTag: Tag = {
+                  id: v4(),
+                  name: formattedTag,
+                };
+                if (tags.find((t) => t.name === newTag.name)) {
+                  return alert("이미 존재하는 태그입니다.");
+                }
+                setTags((prev) => [...prev, newTag]);
                 setTag("");
               }
             }}
-            className={input}
-            placeholder="태그를 입력하세요."
+            className={twMerge("w-full", input)}
+            placeholder="태그를 입력후 shift키를 눌러주세요."
           />
           <div>
             <ul className="flex gap-x-2">
-              {tags.map((t, index) => (
-                <li key={index}>
-                  <button>{t}</button>
+              {tags.map((t) => (
+                <li key={t.id}>
+                  <button>{t.name}</button>
                 </li>
               ))}
             </ul>
           </div>
-          <div>
-            <ul>
-              {files.map((file) => (
-                <li key={file.name}>
-                  <FileItem file={file} />
-                </li>
-              ))}
-              <li>
-                <FileItem />
-              </li>
-            </ul>
-          </div>
-          <div className="flex">
-            <button className="text-black">취소</button>
-            <button className="text-black">게시</button>
-          </div>
-        </form>
-      </div>
+        </div>
+
+        <div className="flex  justify-end gap-x-2.5 absolute bottom-5 right-5">
+          <button
+            type="button"
+            onClick={() => {
+              if (confirm("취소하시겠습니까?")) {
+                navi.back();
+              } else {
+                return alert("취소되었습니다.");
+              }
+            }}
+            className={twMerge("bg-gray-300 ", button)}
+          >
+            취소
+          </button>
+          <button className={twMerge("bg-[rgba(62,188,154)]", button)}>
+            게시
+          </button>
+        </div>
+      </form>
     </div>
   );
 };
@@ -127,3 +183,4 @@ const UploadPostPage = () => {
 export default UploadPostPage;
 
 const input = "bg-white border rounded px-2";
+const button = " rounded px-2.5 py-1";
