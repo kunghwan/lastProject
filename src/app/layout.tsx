4b558b5667543c5c50e31fa9d5436f12b039986c
sank0118 <tks9901@naver.com>
2025-04-25 13:44:44 +0900

    지도 구현중

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Loaiding from "@/components/Loading/page";
+import Script from "next/script";
 
 import BodyLayout from "../components/BodyLayout";
 
@@ -30,11 +31,17 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-white text-black dark:bg-[#333333] dark:text-[#F1F5F9] transition-colors lg:max-w-300 lg:mx-auto`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-white text-black dark:bg-[#333333] dark:text-[#F1F5F9] transition-colors lg:max-w-300 lg:mx-auto `}
       >
         <Loaiding />
         <AuthProvider>
-          <BodyLayout>{children}</BodyLayout>
+          <BodyLayout>
+            <Script
+              src={`https://dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_API_KEY}&autoload=false&libraries=services`}
+              strategy="beforeInteractive"
+            />
+            {children}
+          </BodyLayout>
         </AuthProvider>
       </body>
     </html>
