8a13454f967c28fa99d287050708023f73cef847
wonbinwin <youngkf@hanamil.net>
2025-04-29 14:17:44 +0900

    feed, added & updated PostComponent.tsx

--- src/components/post/ShareButton.tsx
+++ src/components/post/ShareButton.tsx
@@ -1,5 +1,14 @@
+import { GoShareAndroid } from "react-icons/go";
+
 const ShareButton = () => {
-  return <button type="button">ShareButton</button>;
+  return (
+    <button
+      type="button"
+      className="hover:scale-105 cursor-pointer p-0.5 active:text-gray-800 hover:text-blue-400  dark:active:text-blue-500"
+    >
+      <GoShareAndroid />
+    </button>
+  );
 };
 
 export default ShareButton;
