d4eb176448c3c4ecca9a41593d5bba5f0bdc02a0
ykh <ysw03031@naver.com>
2025-05-07 23:46:59 +0900

    ReadMe Update

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -14,7 +14,7 @@ const NotFound = () => {
       </p>
       <button
         onClick={() => navi.push("/")}
-        className="hover:scale-105 hover:animate-pulse flex gap-x-2.5 items-center font-bold p-2.5 rounded w-40 bg-[rgba(62,188,154)] dark:bg-[rgba(116,212,186,0.5)] text-white hover:shadow-md"
+        className="hover:scale-105 hover:animate-pulse flex gap-x-2.5 items-center font-bold p-2.5 rounded w-40 bg-[rgba(62,188,154)] dark:bg-[rgba(116,212,186,0.5)] text-white hover:shadow-md whitespace-nowrap"
       >
         <span>
           <FaArrowLeft className="" />
