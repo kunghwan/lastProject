e0150cc8a97e1616f228648a4ed96bc12d5fcfe2
kunghwan <ysw03031@naver.com>
2025-05-09 11:04:52 +0900

    Login IsPending Update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Loaiding from "@/components/Loading";
+
 import BodyLayout from "../components/BodyLayout";
 
 import { AuthProvider } from "@/contextapi/provider";
@@ -33,7 +33,6 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-white text-black dark:bg-[#333333] dark:text-[#F1F5F9] transition-colors lg:max-w-300 lg:mx-auto`}
       >
         <ReactQueryProvider>
-          <Loaiding />
           <AuthProvider>
             <BodyLayout>{children}</BodyLayout>
           </AuthProvider>
