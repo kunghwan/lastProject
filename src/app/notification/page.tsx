a885c3eb1c65548fc5c84aeec55f7fa4fbbe7c5e
tmdtmddl <tmddlqkqh@naver.com>
2025-05-11 08:58:36 +0900

    모두읽기 버튼 보여주는 조건 바꾸고 알림이 없을때 디자인 변경

--- src/app/notification/page.tsx
+++ src/app/notification/page.tsx
@@ -2,7 +2,7 @@
 import { AUTH } from "@/contextapi/context";
 import { dbService, FBCollection } from "@/lib";
 import { useRouter } from "next/navigation";
-import { useCallback, useEffect, useMemo, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { twMerge } from "tailwind-merge";
 import { useInfiniteQuery } from "@tanstack/react-query";
 import { Notifications } from "@/types/notification";
@@ -10,6 +10,8 @@ import Loaiding from "@/components/Loading";
 import TopButton from "@/components/upplace/TopButton";
 import AlertModal from "@/components/AlertModal";
 import Image from "next/image";
+import { BsExclamationCircle } from "react-icons/bs";
+
 //! limit변수처리하기
 const limit = 20;
 const NotificationListPage = () => {
@@ -191,8 +193,13 @@ const NotificationListPage = () => {
         />
       )}
       {data?.pages.every((page) => page.notifications.length === 0) && (
-        <div className="hsecol justify-center items-center  h-100 gap-y-2.5">
-          <p className="dark:text-white font-bold text-xl">알림이 없습니다.</p>
+        <div className="hsecol justify-center items-center  h-100 gap-y-5">
+          <div>
+            <BsExclamationCircle className="text-4xl text-gray-500" />
+          </div>
+          <p className="dark:text-white font-bold text-xl text-gray-500">
+            아직 도착한 알림이 없어요.
+          </p>
           <button
             onClick={() => navi.back()}
             className="hover:scale-105 hover:animate-pulse font-bold p-2.5 rounded w-40 bg-[rgba(62,188,154)] dark:bg-[rgba(116,212,186,0.5)] text-white hover:shadow-md"
@@ -205,20 +212,18 @@ const NotificationListPage = () => {
       <div>
         {/* isUnRead는 읽지 않은 알림이 하나라도 있으면 true 없다면 false임 */}
         {/* data 안에 있는 pages 배열을 돌면서,알림(notifications)이 하나라도 있는 페이지가 있는지 확인 */}
-        {/* 읽지 않은 알림이 있고, 실제 알림 데이터도 존재할 때만 버튼을 보여줌 */}
-        {isUnRead &&
-          data?.pages.some((page) => page.notifications.length > 0) && (
-            <div className="flex justify-end">
-              {/* isRead가 다 true라면 버튼을 비활성화함 */}
-              <button
-                onClick={handleAllRead}
-                disabled={!isUnRead}
-                className="hover:scale-105 hover:shadow-md border  font-stretch-105% border-lime-800 hover:text-lime-800 cursor-pointer mr-2.5 bg-[#d7eadf] disabled:text-gray-400  disabled:bg-gray-200 dark:bg-[rgba(232,255,241,0.5)] p-2 rounded"
-              >
-                모두 읽기
-              </button>
-            </div>
-          )}
+        {data?.pages.every((page) => page.notifications.length > 0) && (
+          <div className="flex justify-end">
+            {/* isRead가 다 true라면 버튼을 비활성화함 */}
+            <button
+              onClick={handleAllRead}
+              disabled={!isUnRead}
+              className="hover:scale-105 hover:shadow-md border  font-stretch-105% border-lime-800 hover:text-lime-800 cursor-pointer mr-2.5 bg-[#d7eadf] disabled:text-gray-400  disabled:bg-gray-200 dark:bg-[rgba(232,255,241,0.5)] p-2 rounded"
+            >
+              모두 읽기
+            </button>
+          </div>
+        )}
         <ul className=" grid md:grid-cols-2 gap-5  items-center  w-full p-2.5 ">
           {data?.pages.map((page) =>
             page.notifications.map((noti) => (
