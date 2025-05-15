a8e8406fbafc1e6df4867228874b63bd4e579c2f
wonbinwin <youngkf@hanamil.net>
2025-05-15 10:47:11 +0900

    updated username/layout.tsx

--- src/app/profile/[username]/layout.tsx
+++ src/app/profile/[username]/layout.tsx
@@ -1,18 +1,16 @@
-// app/profile/[username]/layout.tsx
-
 import { getUserByUsername } from "@/lib/otherUser";
 import { Metadata } from "next";
 
-export async function generateMetadata({
-  params,
-}: {
+type Props = {
   params: { username: string };
-}): Promise<Metadata> {
+};
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
   const user = await getUserByUsername(params.username);
 
   return {
-    title: `방방콕콕 ${user?.nickname || "알 수 없음"}페이지`,
-    description: `${user?.nickname || "유저"} 마이페이지`,
+    title: `방방콕콕 ${user?.nickname || "알 수 없음"}님 페이지`,
+    description: `${user?.nickname || "유저"}님 마이페이지`,
   };
 }
 
