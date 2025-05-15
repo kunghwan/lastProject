905dd1799d8df4e2e4c1760468e4c86bbfd64b76
kunghwan <ysw03031@naver.com>
2025-05-15 11:20:31 +0900

    init

--- src/app/profile/[username]/page.tsx
+++ src/app/profile/[username]/page.tsx
@@ -6,9 +6,10 @@ import { getUserByUsername } from "@/lib/otherUser";
 export async function generateMetadata({
   params,
 }: {
-  params: { username: string };
+  params: Promise<{ username: string }>;
 }): Promise<Metadata> {
-  const user = await getUserByUsername(params.username);
+  const { username } = await params;
+  const user = await getUserByUsername(username);
   return {
     title: `방방콕콕 ${user?.nickname || "알 수 없음"}님 페이지`,
     description: `${user?.nickname || "유저"}님 마이페이지`,
