9cccde4e577aea79f2229a66b76eb8039716766a
kunghwan <ysw03031@naver.com>
2025-05-19 15:17:51 +0900

    init

--- src/app/idfind/resultid/page.tsx
+++ src/app/idfind/resultid/page.tsx
@@ -4,6 +4,7 @@ import { useCallback, useEffect, useState } from "react";
 import { FaIdCard } from "react-icons/fa6";
 import { TbPassword } from "react-icons/tb";
 import { useAlertModal } from "@/components/AlertStore";
+import FindHeader from "@/components/ui/FindHeader";
 
 const IdFindResult = () => {
   // 상태 정의
@@ -48,22 +49,7 @@ const IdFindResult = () => {
   return (
     <>
       {/* 상단 헤더 - 아이디/비밀번호 찾기 */}
-      <div className="w-full bg-primary p-4 whitespace-nowrap dark:bg-[rgba(116,212,186,0.5)]">
-        <div className="flex md:flex-row items-center gap-4 md:gap-20 p-4 lg:justify-between">
-          <div className="flex items-center w-full md:w-80 gap-2 p-2 rounded">
-            <FaIdCard className="text-amber-500 text-4xl dark:text-amber-700" />
-            <p className="font-bold text-amber-500 dark:text-amber-700">
-              아이디 찾기
-            </p>
-          </div>
-          <div className="flex items-center w-full md:w-80 gap-2 p-2 rounded whitespace-nowrap">
-            <TbPassword className="text-blue-500 text-4xl dark:text-blue-700" />
-            <p className="font-bold text-black  dark:text-white">
-              비밀번호 찾기
-            </p>
-          </div>
-        </div>
-      </div>
+      <FindHeader />
 
       {/* 본문: 이메일 결과 표시 영역 */}
       <div className="w-full flex justify-center flex-col ml-5">
