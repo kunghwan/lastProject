80a073a08e5b9b7ce8650a184f36a2aaeda6d658
kunghwan <ysw03031@naver.com>
2025-05-22 12:31:02 +0900

    init

--- src/app/pwfind/page.tsx
+++ src/app/pwfind/page.tsx
@@ -261,14 +261,14 @@ const PwFindResult = () => {
       <div className="w-full bg-primary p-4 whitespace-nowrap dark:bg-[rgba(116,212,186,0.5)]  ">
         <div className="flex md:flex-row items-center gap-4 md:gap-20 p-4 lg:justify-between">
           <div className="flex items-center w-full md:w-80 gap-2 p-2 rounded">
-            <FaIdCard className="text-amber-500 text-4xl dark:text-amber-700" />
-            <p className="font-bold text-black dark:text-white">아이디 찾기</p>
+            <FaIdCard className="text-emerald-500 text-4xl dark:text-emerald-600" />
+            <p className="font-bold text-black  dark:text-white">아이디 찾기</p>
           </div>
           <div className="flex items-center w-full md:w-80 gap-2 p-2 rounded">
-            <TbPassword className="text-blue-500 text-4xl dark:text-blue-700" />
+            <TbPassword className="text-emerald-500 dark:text-emerald-600  text-4xl" />
             <Link
               href="/pwfind"
-              className="font-bold text-black-500  whitespace-nowrap text-amber-500 dark:text-amber-700 "
+              className="font-bold text-black-500  whitespace-nowrap text-emerald-700 dark:text-emerald-800 "
             >
               비밀번호 찾기
             </Link>
